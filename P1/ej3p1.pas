e68bc19e58d3f7ad3715c2f0c54a2311118a173d
Lucero Gauna <71735200+Lucerogauna@users.noreply.github.com>
2025-04-02 10:50:41 -0300

    Update ej3p1.pas

--- P1/ej3p1.pas
+++ P1/ej3p1.pas
@@ -1,90 +1,38 @@
-{ 3. Realizar un programa que presente un menú con opciones para:
- a. Crear un archivo de registros no ordenados de empleados y completarlo con
- datos ingresados desde teclado. De cada empleado se registra: número de
- empleado, apellido, nombre, edad y DNI. Algunos empleados se ingresan con
- DNI 00. La carga finaliza cuando se ingresa el String ‘fin’ como apellido.
- b. Abrir el archivo anteriormente generado y
- i.
- ii.
- iii.
- Listar en pantalla los datos de empleados que tengan un nombre o apellido
- determinado, el cual se proporciona desde el teclado.
- Listar en pantalla los empleados de a uno por línea.
- Listar en pantalla los empleados mayores de 70 años, próximos a jubilarse.
- NOTA: El nombre del archivo a crear o utilizar debe ser proporcionado por el usuario.
+{
+   e3p1nuevo.pas
+   
+   Copyright 2025 Usuario <Usuario@DESKTOP-U6ORN85>
+   
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+   MA 02110-1301, USA.
    
    
 }
 
 
-program ej3p1;
-type 
-	empleado = record
+program ej3p1nuevo;
+type
+	empleados= record
 		nro: integer;
-		apellido: string[20];
-		nombre: string[20];
+		apellido: string;
+		nombre: string;
 		edad: integer;
 		dni: integer;
-	end;
-	lista= ^nodo;
-	nodo= record
-		dato: empleado;
-		sig: lista;
-	end;
-	archivoDeEmpleados= file of empleado;
-procedure imprimirRegistro (e: empleado);
-begin
-	with e do begin
-	writeln('Nombre: ',nombre);
-	writeln('Apellido: ', apellido );
-	writeln('Nro: ', nro);
-	writeln('Edad: ',edad);
-	writeln('Dni: ', dni);
-end;
-end;
-procedure imprimirArchivo(var a: archivoDeEmpleados);
-var 
-	e: empleado;
-begin
-	Reset(a);
-	while not eof (a) do begin
-		Read(a, e); //lee el reg del archivo
-		imprimirRegistro(e);// imprime
-	end;
-	close(a);
-end;
-procedure cargarL (var l: lista; e: empleado);
-var
-	nue: lista;
-begin
-	new(nue);
-	nue^.dato:=e;
-	nue^.sig:= l;
-	l:= nue; 
-end;
-procedure imprimirLista(l: lista);
-begin
-	while (l<> nil) do begin
-		imprimirRegistro(l^.dato);
-		l:= l^.sig;
-	end;
-end;
-procedure buscar(x: string; var a: archivoDeEmpleados; var l: lista);
-var 
-	e: empleado;
-begin
-	Reset(a);
-	while not eof(a) do begin
-		Read(a, e);
-		if ((e.nombre = x) or (e.apellido = x)) then
-			cargarL(l, e);
-	end;
-	close(a);
-	writeln('imprimirLista');
-	imprimirLista(l);
-end;
-
-procedure leerE (var e: empleado);
+		end;
+	archE = file of Empleados;
+procedure leerE (var e: empleados);
 begin
 	with e do begin
 		writeln('leer apellido del empleado');
@@ -101,29 +49,91 @@ begin
 		end;
 	end;
 end;
-var
-	arch_name: string[30]; //archivo Fisico
-	arch_log: archivoDeEmpleados;
-	e: empleado;
-	l: lista;
-	nombreoapellido: string;
-BEGIN	
-	l:= nil;
-	writeln('leer nombre del archivo');
-	readln(arch_name);
-	assign(arch_log, arch_name);
-	rewrite(arch_log); //creacion del archivo, puntero en pos 0
-	leerE(e);
-	while (e.apellido <> 'fin') do begin
-		write(arch_log, e);
-		leerE(e);
+
+procedure imprimirRegistro (e: empleados);
+begin
+	with e do begin
+	writeln('Nombre: ',nombre);
+	writeln('Apellido: ', apellido );
+	writeln('Nro: ', nro);
+	writeln('Edad: ',edad);
+	writeln('Dni: ', dni);
+end;
+end;	
+procedure incisoB(var a: archE);
+var e: empleados;
+x: string;
+begin
+	Writeln('leer nombre o apellido');
+	readln(x);
+	reset (a);
+	while not eof(a) do begin
+		read(a, e);
+		if (e.apellido = x) or (e.nombre = x) then begin
+			seek (a, filepos(a)-1);
+			imprimirRegistro(e);
+		end;
 	end;
-	close(arch_log);
-	writeln('leer un nombre o apellido');
-	readln(nombreoapellido);
-	buscar(nombreoapellido, arch_log, l); //inciso i
-	writeln('/////////////////////');
-	writeln('ARCHIVO COMPLETO');
-	imprimirArchivo(arch_log); //inciso ii
+end;
+	
+procedure incisoB3(var a: archE);
+var e: empleados;
+begin
+	reset (a);
+	while not eof(a) do begin
+		read(a, e);
+		if (e.edad >70) then begin
+			seek (a, filepos(a)-1);
+			imprimirRegistro(e);
+		end;
+	end;
+end;	
+procedure incisoB2(var a: archE);
+var e: empleados;
+begin
+	reset (a);
+	while not eof(a) do begin
+		read(a, e);
+		seek (a, filepos(a)-1);
+		imprimirRegistro(e);
+		end;
+	end;
+
+procedure verInciso (var a: archE);
+var 
+	op: integer;
+begin
+	writeln('marque 1:listarDatosDeEmpleadoX, 2 listarTodos, 3: listar > 70 años');
+	read(op);
+	case op of 
+		1: incisoB(a);
+		2: incisoB2(a);
+		3: incisoB3(a);
+	end;
+end;
+VAR
+	archLog: archE;
+	archFis: String;
+	e: empleados;
+	op: integer;
+	
+BEGIN
+	writeln('leer nombre arch');
+	readln(archFis);
+	assign(archLog, archFis);
+	writeln('op1: crear: op2: abrir');
+	read(op);
+	case op of
+	1 : begin
+		rewrite (archLog);
+		leerE(e);
+		while (e.apellido <> 'fin') do begin
+			Write (archLog, e);
+			leerE(e);
+	end;
+	end;
+	2: verInciso(archLog);
+	end;
+	close (archLog);
 END.
 
